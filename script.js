dd54507198f4405efe71835ebdbd62eaea2446c3
lucas <schoenenbergerl@bzz.ch>
2023-10-02 13:59:49 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {document.getElementById('greetButton');
+
+    document.getElementById('greetButton').addEventListener('click', function () {
+        alert('Hallo Welt');
+    });
+});
